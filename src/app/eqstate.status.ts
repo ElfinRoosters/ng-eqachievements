338a88342bc28f967bdcde32523f233bfe50c78e
ElfinRoosters <[email protected]>
2025-01-25 09:37:37 -0600

    add convertToEQStateEnum function

--- src/app/eqstate.status.ts
+++ src/app/eqstate.status.ts
@@ -3,3 +3,24 @@ export enum EQState {
     Completed,
     Locked
 }
+
+export function convertToEQStateEnum(str: string): EQState {
+    var state = EQState[str as keyof typeof EQState];
+    if (state !== undefined) { return state; }
+
+    switch (str) {
+        case 'L':
+            state = EQState.Locked;
+            break;
+        case 'C':
+            state = EQState.Completed;
+            break;
+        case 'I':
+        default:
+            state = EQState.Incomplete;
+            break;
+
+    }
+
+    return state;
+}
